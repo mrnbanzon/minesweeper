d80786adc25ece92836541122b3a7d55e307d456
Michael Banzon <mrnbanzon@gmail.com>
2018-01-17 21:49:43 -0800

    Added the first part to codecademy's terminal minesweeper.

---
+++ src/minesweeper.js
@@ -0,0 +1,10 @@
+const blankline = '  |   |  ';
+
+console.log('This is what an empty board would look like:');
+console.log(blankline + '\n' + blankline + '\n' + blankline);
+
+const guessline = '1 |   |  ';
+const bombline = '  | B |  ';
+
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(`${guessline}\n${bombline}\n${blankline}`);
