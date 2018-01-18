0819aee9562735560ff6879d368552887ddf8d5f
Michael Banzon <mrnbanzon@gmail.com>
2018-01-17 21:52:06 -0800

    Refactored Part 1 a bit.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,10 +1,9 @@
 const blankline = '  |   |  ';
+const guessline = '1 |   |  ';
+const bombline = '  | B |  ';
 
 console.log('This is what an empty board would look like:');
 console.log(blankline + '\n' + blankline + '\n' + blankline);
-
-const guessline = '1 |   |  ';
-const bombline = '  | B |  ';
 
 console.log('This is what a board with a guess and a bomb on it would look like:');
 console.log(`${guessline}\n${bombline}\n${blankline}`);
