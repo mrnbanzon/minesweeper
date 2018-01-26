b00583ab1118e62d99fab5ed0ab0572e3734df9a
Michael Banzon <mrnbanzon@gmail.com>
2018-01-26 10:39:15 -0800

    Starting Fresh for part 2.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,9 +1,1 @@
-const blankline = '  |   |  ';
-const guessline = '1 |   |  ';
-const bombline = '  | B |  ';
-
-console.log('This is what an empty board would look like:');
-console.log(blankline + '\n' + blankline + '\n' + blankline);
-
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(`${guessline}\n${bombline}\n${blankline}`);
+let board = [];
